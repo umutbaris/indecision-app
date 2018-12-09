3418f626de88c81ab2c5789cb9736b152b081285
Karasar, Umut <umut.karasar@sony.com>
2018-12-09 22:44:38 +0300

    master Components converts to stateless functional components

--- src/app.js
+++ src/app.js
@@ -57,53 +57,45 @@ class IndecisionApp extends React.Component {
   }
 }
 
-class Header extends React.Component {
-  render() {
+const Header = (props) => {
 	return (
-	  <div>
-		<h1>{this.props.title}</h1>
-		<h2>{this.props.subtitle}</h2>
-	  </div>
-	);
-  }
-}
+		<div>
+		  <h1>{props.title}</h1>
+		  <h2>{props.subtitle}</h2>
+		</div>
+	  );
+};
 
-class Action extends React.Component {
-  render() {
+const Action = (props) => {
 	return (
-	  <div>
-		<button 
-		onClick={this.props.handlePick}
-		disabled={!this.props.hasOptions}
-		>
-		What should I do?
-		</button>
-	  </div>
-	);
-  }
-}
+		<div>
+		  <button 
+		  onClick={props.handlePick}
+		  disabled={!props.hasOptions}
+		  >
+		  What should I do?
+		  </button>
+		</div>
+	  );
+};
 
-class Options extends React.Component {
-  render() {
+const Options = (props) => {
 	return (
-	  <div>
-		<button onClick={this.props.handleDeleteOptions}>Remove All</button>
-		{
-		  this.props.options.map((option) => <Option key={option} optionText={option} />)
-		}
-	  </div>
-	);
-  }
-}
+		<div>
+		  <button onClick={props.handleDeleteOptions}>Remove All</button>
+		  {
+			props.options.map((option) => <Option key={option} optionText={option} />)
+		  }
+		</div>
+	  );
+};
 
-class Option extends React.Component {
-  render() {
+const Option = (props) => {
 	return (
-	  <div>
-		{this.props.optionText}
-	  </div>
-	);
-  }
+		<div>
+		  {props.optionText}
+		</div>
+	  );
 }
 
 class AddOption extends React.Component {
@@ -139,5 +131,15 @@ class AddOption extends React.Component {
 	);
   }
 }
+// const User = () => {
+// 	return(
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+// 		<div>
+// 			<p> Name : </p>
+// 			<p> Age : </p>
+// 		 </div>
+
+// 	);
+// }
+
+ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
