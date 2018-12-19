9ba8363858d537ba97d9be57b722f6f4ebef95a9
Karasar, Umut <umut.karasar@sony.com>
2018-12-19 21:42:51 +0300

    master Adding npm modules

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
-import { square, add } from './utils.js';
-import { isAdult, canDrink } from './person.js';""
-console.log("app.js is running");
-console.log(square(4));
-console.log(add(5,5));
-console.log(isAdult(19));
-console.log(canDrink(19));
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = React.createElement('p', {}, 'testing 123');
+ReactDOM.render(template, document.getElementById('app'));
